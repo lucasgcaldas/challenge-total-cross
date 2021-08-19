c754ae00a32a6e658628468e17d353c9de6c46f4
Lucas Gomes <lggomes007@gmail.com>
2021-08-18 22:48:27 -0300

    Adding ConsecutiveNumbers exercise (difficult hard)

---
+++ src/application/ConsecutiveNumbers.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.Arrays;
+
+public class ConsecutiveNumbers {
+
+	public static void main(String[] args) {
+
+		int vet1[] = { 5, 1, 4, 3, 2};
+		int vet2[] = {5, 1, 4, 3, 2, 8};
+		int vet3[] = {5, 6, 7, 8, 9, 9};
+
+		cons(vet1);
+		cons(vet2);
+		cons(vet3);
+	}
+
+	public static void cons(int vet[]) {
+
+		Arrays.sort(vet);
+		boolean consec = true;
+
+		for (int i = 0; i < vet.length; i++) {
+			if (i + 1 != vet.length) {
+				if (vet[i] + 1 != vet[i + 1]) {
+					consec = false;
+					break;
+				}
+			}
+		}
+		System.out.println(consec);
+	}
+}
